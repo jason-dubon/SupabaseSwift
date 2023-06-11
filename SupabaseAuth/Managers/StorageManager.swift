ed1416d7e44df6fb236f20a6bdeb2c7e002b4b6b
Jason Dubon <jasonsdubon@gmail.com>
2023-06-11 11:43:00 -0400

    Update StorageManager.swift

--- SupabaseAuth/Managers/StorageManager.swift
+++ SupabaseAuth/Managers/StorageManager.swift
@@ -13,11 +13,7 @@ class StorageManager {
     
     static let shared = StorageManager()
     
-    private let apikey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml4amZ2Y2RtcW5oZnpkaHBjand2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODM3MjQ2MTksImV4cCI6MTk5OTMwMDYxOX0.JYC-8ibgH6ILDCFozR_dDzlGcPE6v2u1e3WQXza9Ni4"
-    
-    private let secret = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml4amZ2Y2RtcW5oZnpkaHBjand2Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY4MzcyNDYxOSwiZXhwIjoxOTk5MzAwNjE5fQ.e8EhHSa7KK-2eiiwEHSdu_xKlfudgZ6kkK8aud2PvjI"
-    
-    lazy var storage = SupabaseStorageClient(url: "https://ixjfvcdmqnhfzdhpcjwv.supabase.co/storage/v1", headers: [
+    lazy var storage = SupabaseStorageClient(url: "https://SERVER_DETAILS.supabase.co/storage/v1", headers: [
         "Authorization": "Bearer \(secret)",
         "apikey" : apikey,
     ])
